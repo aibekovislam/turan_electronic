aa135d078f8c5035b2aa7eb971e0ad4e1cb72067
aibekovislam <islamxdaamduu@gmail.com>
2024-02-23 20:18:41 +0600

    navbar proccess

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import "../styles/main.scss"
 
 function MainLayout() {
   return (
-    <div>
+    <div className="container">
       <Navbar />
       <Outlet />
     </div>
