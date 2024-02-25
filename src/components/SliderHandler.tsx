13cdd76cac18c70d4b2abaf3178db32b0c45da9f
aibekovislam <islamxdaamduu@gmail.com>
2024-02-25 13:55:54 +0600

    added some options for carousel and added text under img

--- src/components/SliderHandler.tsx
+++ src/components/SliderHandler.tsx
@@ -2,7 +2,6 @@ import Slider from "react-slick";
 import prevArrowSVG from "../assets/svgs/Vector (5).svg";
 import nextArrowSVG from "../assets/svgs/Vector (6).svg";
 import styles from "../styles/slider.module.scss";
-import React from "react";
 
 function SampleNextArrow(props: any) {
     const { className, style, onClick } = props;
@@ -27,8 +26,7 @@ export default function SimpleSlider() {
         slidesToScroll: number;
         nextArrow: JSX.Element;
         prevArrow: JSX.Element;
-        appendDots: (dots: JSX.Element) => JSX.Element;
-        customPaging: (i: number) => JSX.Element;
+        dotsClass: string;
     } = {
         dots: true,
         infinite: true,
@@ -37,36 +35,22 @@ export default function SimpleSlider() {
         slidesToScroll: 1,
         nextArrow: <SampleNextArrow />,
         prevArrow: <SamplePrevArrow />,
-        appendDots: dots => (
-            <div style={{ position: "absolute", bottom: "10px" }} >
-                <ul style={{ margin: "0px" }}> {dots} </ul>
-            </div>
-        ),             
-        customPaging: () => (
-            <div
-                style={{
-                    "width": "8px", 
-                    "height": "8px", 
-                    "borderRadius": "50%",
-                    "backgroundColor": "#D9D9D9" 
-                }}>
-            </div>
-        )
+        dotsClass: `slick-dots ${styles.dots_c}`
     };    
   return (
     <div className={styles.carousel} >
         <Slider {...settings}>
-            <div>
+            <div className={styles.carousel__item}>
                 <img src="https://s3-alpha-sig.figma.com/img/a168/6110/47f9cdb61d3cfea251c8edf680705f72?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PFwuEdogxXr9Hk7BjKvnomoPFEYjfR0tjfKPTSVanFibJo9VADi8kgv4GP6Wogab4fU~5cUF35Xy0D1i1E3gSclqawtkLLmJOlLve33uzK1BDDfD3n-~bTRkGSe67P4~ER845g92dnwAGq-4RUYoee1gH23ifMpDcJCGsoETG0M2gm6LADQZF9~39Hu6XsXKxykCf68LHAW3xhMAtwRzNjMJ6qph4Y~q9CiGMLvn~mcGNWsiz7sq4AgoZO24nF~LnaSQ3Gqq9xqG7rlSM9OIIWniAlxn4XeYHfMk8Hgv7xT5uYznwF8wD1hi5SwxJEiXZh8EcrH522DY6LBdeD15OA__" className={styles.carousel__img} />
+                <div className={styles.text__carousel}>Some text 1</div>
             </div>
-            <div>
+            <div className={styles.carousel__item}>
                 <img src="https://s3-alpha-sig.figma.com/img/a168/6110/47f9cdb61d3cfea251c8edf680705f72?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PFwuEdogxXr9Hk7BjKvnomoPFEYjfR0tjfKPTSVanFibJo9VADi8kgv4GP6Wogab4fU~5cUF35Xy0D1i1E3gSclqawtkLLmJOlLve33uzK1BDDfD3n-~bTRkGSe67P4~ER845g92dnwAGq-4RUYoee1gH23ifMpDcJCGsoETG0M2gm6LADQZF9~39Hu6XsXKxykCf68LHAW3xhMAtwRzNjMJ6qph4Y~q9CiGMLvn~mcGNWsiz7sq4AgoZO24nF~LnaSQ3Gqq9xqG7rlSM9OIIWniAlxn4XeYHfMk8Hgv7xT5uYznwF8wD1hi5SwxJEiXZh8EcrH522DY6LBdeD15OA__" className={styles.carousel__img} />
+                <div className={styles.text__carousel}>Some text 2</div>
             </div>
-            <div>
+            <div className={styles.carousel__item}>
                 <img src="https://s3-alpha-sig.figma.com/img/a168/6110/47f9cdb61d3cfea251c8edf680705f72?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PFwuEdogxXr9Hk7BjKvnomoPFEYjfR0tjfKPTSVanFibJo9VADi8kgv4GP6Wogab4fU~5cUF35Xy0D1i1E3gSclqawtkLLmJOlLve33uzK1BDDfD3n-~bTRkGSe67P4~ER845g92dnwAGq-4RUYoee1gH23ifMpDcJCGsoETG0M2gm6LADQZF9~39Hu6XsXKxykCf68LHAW3xhMAtwRzNjMJ6qph4Y~q9CiGMLvn~mcGNWsiz7sq4AgoZO24nF~LnaSQ3Gqq9xqG7rlSM9OIIWniAlxn4XeYHfMk8Hgv7xT5uYznwF8wD1hi5SwxJEiXZh8EcrH522DY6LBdeD15OA__" className={styles.carousel__img} />
-            </div>
-            <div>
-                <img src="https://s3-alpha-sig.figma.com/img/a168/6110/47f9cdb61d3cfea251c8edf680705f72?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PFwuEdogxXr9Hk7BjKvnomoPFEYjfR0tjfKPTSVanFibJo9VADi8kgv4GP6Wogab4fU~5cUF35Xy0D1i1E3gSclqawtkLLmJOlLve33uzK1BDDfD3n-~bTRkGSe67P4~ER845g92dnwAGq-4RUYoee1gH23ifMpDcJCGsoETG0M2gm6LADQZF9~39Hu6XsXKxykCf68LHAW3xhMAtwRzNjMJ6qph4Y~q9CiGMLvn~mcGNWsiz7sq4AgoZO24nF~LnaSQ3Gqq9xqG7rlSM9OIIWniAlxn4XeYHfMk8Hgv7xT5uYznwF8wD1hi5SwxJEiXZh8EcrH522DY6LBdeD15OA__" className={styles.carousel__img} />     
+                <div className={styles.text__carousel}>Some text 3</div>
             </div>
         </Slider>
     </div>
