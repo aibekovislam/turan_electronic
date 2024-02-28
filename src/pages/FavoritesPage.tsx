a9bea267bc18e30462940afc0121216dfd5554b4
Baimanchick <baika132213@gmail.com>
2024-02-28 17:03:42 +0600

    fixed bags / favorite finished

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,16 +1,12 @@
 import Brands from "../components/Brands"
-import FavoriteEmpty from "../components/FavoriteEmpty"
 import FavoriteList from "../components/FavoriteList"
 import Footer from "../components/Footer"
-import RecommendationList from "../components/RecommendationList"
 import SimpleSlider from "../components/SliderHandler"
 
 function FavoritesPage() {
 
   return (
-    <div>
-        {/* <FavoriteEmpty/>
-        <RecommendationList/> */}
+    <div> 
         <SimpleSlider/>
         <FavoriteList/>
         <Brands/>
