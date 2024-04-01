7a1a35442ef1ec91bf5df81c2d6049b8110590e9
aibekovislam <islamxdaamduu@gmail.com>
2024-04-01 11:12:52 +0600

    modified mini card list

--- src/components/Brands.tsx
+++ src/components/Brands.tsx
@@ -1,9 +1,4 @@
 import styles from "../styles/brands_and_footer.module.scss";
-// import apple from "../assets/svgs/brands/image 33.svg";
-// import dyson from "../assets/svgs/brands/image 34.svg";
-// import playstation from "../assets/svgs/brands/image 35.svg";
-// import samsung from "../assets/svgs/brands/image 36.svg";
-// import xiaomi from "../assets/svgs/brands/image 37.svg";
 import { useDispatch, useSelector } from "react-redux";
 import { RootStates } from "../store/store";
 import { useEffect } from "react";
