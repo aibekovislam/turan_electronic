3c896f602cbd596743bbf0e67f4a60d4aa56263b
Islam Aibekov <78363252+aibekovislam@users.noreply.github.com>
2024-04-21 15:00:01 +0600

    fix bug with card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -138,7 +138,7 @@ const Card: React.FC<CardProps> = ({ product, onClick }) => {
                                 )}
                                 <div className={styles.price_mobile}>
                                     { product.price !== 0 ? (
-                                        {product.price} сом
+                                        product.price сом
                                     ) : (
                                         <div>Нету в наличии</div>
                                     ) }
