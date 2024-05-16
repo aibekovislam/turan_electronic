d746c5c6ffe865019931e1e991ce6e9a479cea28
aibekovislam <islamxdaamduu@gmail.com>
2024-05-16 14:00:46 +0600

    modified images

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -1,19 +1,19 @@
 import styles from "../styles/slider.module.scss"
 import { API_URL } from "../utils/consts";
 
-export function ImageThumbnail({ image, index, handleClick, isSelected }: any) {
+export function ImageThumbnail({ image, index, handleClick, isSelected, product }: any) {
     return (
       <div className={styles.thumbnail}>
         <img
+          loading="lazy"
+          height={300}
+          width={300}
           className={isSelected ? styles.clicked : styles.detail_img__item}
           src={`${API_URL}${image}`}
           onClick={() => handleClick(index)}
-          alt="Product Image"
-          title="Product Image"
+          alt={`Купить ${product.brand_title} ${product.name} в Бишкеке`}
+          title={`Купить ${product.brand_title} ${product.name} в Бишкеке`}
           fetchPriority="high"
-          decoding="async"
-          width={300}
-          height={300}
         />
       </div>
     );
